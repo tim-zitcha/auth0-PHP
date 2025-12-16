0d78289d9ed0028e8832aca664d3c25d164972a1
Snehil Kishore <snehil.kishore@okta.com>
2025-12-16 18:00:04 +0530

    Release 8.18.0 (#803)
    ### Changes
    
    Security fix: Resolve CVE-2025-68129

--- tests/Utilities/TokenGenerator.php
+++ tests/Utilities/TokenGenerator.php
@@ -26,7 +26,7 @@ class TokenGenerator
             'sid' => '__test_sid__',
             'iss' => 'https://domain.test/',
             'aud' => '__test_client_id__',
-            // 'nonce' => '__test_nonce__', Only ID tokens should have nonce claims
+            'nonce' => '__test_nonce__',
             'auth_time' => time() - 100,
             'exp' => time() + 1000,
             'iat' => time() - 1000,
@@ -43,7 +43,7 @@ class TokenGenerator
             'iss' => 'https://domain.test/',
             'sid' => '__test_sid__',
             'aud' => '__test_client_id__',
-            'nonce' => '__test_nonce__',
+            // 'nonce' => '__test_nonce__', Only ID tokens should have nonce claims
             'auth_time' => time() - 100,
             'exp' => time() + 1000,
             'iat' => time() - 1000,
